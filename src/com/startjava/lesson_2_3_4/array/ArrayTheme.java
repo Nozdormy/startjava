40a5e2e750819461387c4a7cac5c12327dc9509d
Sergey <goncharov96snz@gmail.com>
2022-11-07 15:26:56 +0500

    Edit HW

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_2_3.array;
+package com.startjava.lesson_2_3_4.array;
 
 public class ArrayTheme {
     public static void main(String[] args) {
@@ -6,10 +6,11 @@ public class ArrayTheme {
         int[] intArray = {1, 7, 2, 6, 3, 5, 4};
         int len = intArray.length;
         show(intArray);
-        for (int i = 0; i < len / 2; i++) {
+        for (int i = 0; i <= len / 2; i++) {
+            len--;
             int temp = intArray[i];
-            intArray[i] = intArray[len - i - 1];
-            intArray[len - i - 1] = temp;
+            intArray[i] = intArray[len];
+            intArray[len] = temp;
         }
         show(intArray);
 
@@ -19,17 +20,13 @@ public class ArrayTheme {
         for (int i = 0; i < len; i++) {
             intArray[i] = i;
         }
-        show(intArray);
-        int sum = 1;
-        String str;
+        int mult = 1;
         for (int i = 1; i < len - 1; i++) {
-            str = (i == 8 ? i + " = " : i + " * ");
-            System.out.print(str);
-            sum *= i;
+            System.out.print(i + (i == 8 ? " = " : " * "));
+            mult *= i;
         }
-        System.out.println(sum);
-        int sum2 = intArray[0] * intArray[9];
-        System.out.println(sum2 + "\n");
+        System.out.println(mult);
+        System.out.println(intArray[0] + "\n" + intArray[9] + "\n");
 
         System.out.println("3. Удаление элементов массива");
         double[] doubleArray = new double[15];
@@ -38,15 +35,15 @@ public class ArrayTheme {
             doubleArray[i] = Math.random();
         }
         show(doubleArray);
-        int midleArray = len / 2;
-        System.out.println("Индекс средней ячейки - " + midleArray);
+        double midleArray = doubleArray[len / 2];
         int count = 0;
         for (int i = 0; i < len; i++) {
-            if (doubleArray[midleArray] < doubleArray[i]) {
+            if (midleArray > doubleArray[i]) {
                 doubleArray[i] = 0;
                 count++;
             }
         }
+        System.out.println();
         show(doubleArray);
         System.out.println("Количество обнуленных ячеек - " + count + "\n");
 
@@ -56,7 +53,7 @@ public class ArrayTheme {
         for (int i = 0; i < len; i++) {
             alphabet[i] = (char) ('A' + i);
         }
-        str = "";
+        String str = "";
         for (int i = 25; i >= 0; i--) {
             str += alphabet[i];
             System.out.println(str);
@@ -99,8 +96,8 @@ public class ArrayTheme {
         show(stringArray);
         len = stringArray.length;
         count = 0;
-        for (String s: stringArray) {
-            if(!s.isBlank()) {
+        for (String s : stringArray) {
+            if (!s.isBlank()) {
                 count++;
             }
         }
@@ -133,10 +130,9 @@ public class ArrayTheme {
     private static void show(double[] array) {
         for (int i = 0; i < array.length; i++) {
             if (i == 8) {
-                System.out.printf("%n%11.3f ", array[i]);
-            } else {
-                System.out.printf("%.3f ", array[i]);
+                System.out.println();
             }
+            System.out.printf("%.3f ", array[i]);
         }
         System.out.println();
     }
