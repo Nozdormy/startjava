bc10dba84fdb46a80ed9677ef25d42672655da9d
Sergey <goncharov96snz@gmail.com>
2022-11-16 23:22:13 +0500

    Edit_2 Calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,23 +2,23 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public static void mathExpression(String str) {
-        String[] expression = str.split(" ");
-        int num1 = Integer.parseInt(expression[0]);
-        char sign = expression[1].charAt(0);
-        int num2 = Integer.parseInt(expression[2]);
-        if (num1 >= 0 && num2 >= 0) {
-            switch (sign) {
-                case '+' -> System.out.println(Math.addExact(num1, num2));
-                case '-' -> System.out.println(Math.subtractExact(num1, num2));
-                case '*' -> System.out.println(Math.multiplyExact(num1, num2));
-                case '/' -> System.out.println(Math.floorDiv(num1, num2));
-                case '%' -> System.out.println(Math.floorMod(num1, num2));
-                case '^' -> System.out.println((int) Math.pow(num1, num2));
-                default -> System.out.println("Неверный символ математической операции");
-            }
+    public static int calculate(String expression) {
+        String[] partsExpression = expression.split(" ");
+        int num1 = Integer.parseInt(partsExpression[0]);
+        char sign = partsExpression[1].charAt(0);
+        int num2 = Integer.parseInt(partsExpression[2]);
+        if (num1 < 1 || num2 < 1) {
+            throw new IllegalStateException("Введите положительные числа");
         } else {
-            System.out.println("Введите положительные числа");
+            return switch (sign) {
+            case '+' -> Math.addExact(num1, num2);
+            case '-' -> Math.subtractExact(num1, num2);
+            case '*' -> Math.multiplyExact(num1, num2);
+            case '/' -> Math.floorDiv(num1, num2);
+            case '%' -> Math.floorMod(num1, num2);
+            case '^' -> (int) Math.pow(num1, num2);
+            default -> throw new IllegalStateException ("Неверный символ математической операции");
+        };
         }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -9,24 +9,24 @@ public class CalculatorTest {
         String answer = "yes";
         do {
             if (answer.equals("yes")) {
-                calculate();
+                launchСalculate();
             }
             System.out.println("Хотите продолжить вычисления? Введите yes/no");
             answer = scan.nextLine();
         } while (!answer.equals("no"));
     }
 
-    private static void calculate() {
+    private static void launchСalculate() {
         Scanner scan = new Scanner(System.in);
         try {
             System.out.print("Введите математическое выражение: ");
-            Calculator.mathExpression(scan.nextLine());
+            System.out.println(Calculator.calculate(scan.nextLine()));
         } catch (NumberFormatException e) {
-            System.out.println("Введите корректные значения");
-        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Введите корректные значения");
         } catch (ArithmeticException e) {
             System.out.println("На 0 нельзя делить");
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
